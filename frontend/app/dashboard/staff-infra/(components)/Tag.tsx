faff8b70164bd50e468a4dc0576e73d963b51b34
Ayoub Chaaoui <ayoubchaaoui02@gmail.com>
2023-08-30 19:45:58 +0100

    refactor tag variant classses

--- frontend/app/dashboard/staff-infra/(components)/Tag.tsx
+++ frontend/app/dashboard/staff-infra/(components)/Tag.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { PropsWithChildren, useState } from "react";
 
 type TagProps = PropsWithChildren & {
@@ -8,24 +9,24 @@ type TagProps = PropsWithChildren & {
 
 export default function Tag({ children, variant, onChange = null }: TagProps) {
   const [check, setCheck] = useState(false);
-  let variantClass = "";
-  if (variant == "success")
-    variantClass = "border-b-green-500 " + (check ? "bg-green-100" : "");
-  if (variant == "warning")
-    variantClass = "border-b-yellow-500 " + (check ? "bg-yellow-100" : "");
-  if (variant == "danger")
-    variantClass = "border-b-red-500 " + (check ? "bg-red-100" : "");
-  if (variant == "risk")
-    variantClass = "border-b-red-400 " + (check ? "bg-red-100" : "");
-  if (variant == "other")
-    variantClass = "border-b-gray-500 " + (check ? "bg-gray-200" : "");
+  let variants = {
+    success: `border-b-green-500 ${check ? "bg-green-100" : ""}`,
+    warning: `border-b-yellow-500 ${check ? "bg-yellow-100" : ""}`,
+    danger: `border-b-red-500 ${check ? "bg-red-100" : ""}`,
+    risk: `border-b-red-400 ${check ? "bg-red-100" : ""}`,
+    other: `border-b-gray-500 ${check ? "bg-gray-200" : ""}`,
+  };
+
   return (
     <button
       onClick={() => {
         setCheck((value) => !value);
         if (onChange) onChange();
       }}
-      className={`inline-flex items-center rounded border px-2.5 py-0.5 text-xs bg-white font-semibold border-b-2 ${variantClass}`}
+      className={cn(
+        "bg-white inline-flex items-center rounded border px-2.5 py-0.5 text-xs  font-semibold border-b-2",
+        variants[variant]
+      )}
     >
       {children}
     </button>
